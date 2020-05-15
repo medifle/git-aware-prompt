6808aa1ea8c48f5b3a6dfe9592c6154954754652
medifle <13296643+medifle@users.noreply.github.com>
2020-05-15 01:10:01 -0400

    add a git status toggle; add shortend working dir;

--- prompt.sh
+++ prompt.sh
@@ -1,32 +1,45 @@
-find_git_branch() {
-  # Based on: http://stackoverflow.com/a/13003854/170413
+# https://github.com/jimeh/git-aware-prompt/pull/67
+NEED_GIT_STATUS=true
+
+calc_git() {
   local branch
   if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
     if [[ "$branch" == "HEAD" ]]; then
       branch='detached*'
     fi
     git_branch="($branch)"
+    if [[ $NEED_GIT_STATUS ]]; then
+      local status=$(git status --porcelain 2> /dev/null)
+      if [[ "$status" != "" ]]; then
+        git_dirty='*'
+      else
+        git_dirty=''
+      fi
+    fi
   else
-    git_branch=""
-  fi
-}
-
-find_git_dirty() {
-  local status=$(git status --porcelain 2> /dev/null)
-  if [[ "$status" != "" ]]; then
-    git_dirty='*'
-  else
+    git_branch=''
     git_dirty=''
   fi
 }
 
-PROMPT_COMMAND="find_git_branch; find_git_dirty; $PROMPT_COMMAND"
+function shortwd() {
+  num_dirs=3
+  pwd_symbol="..."
+  newPWD="${PWD/#$HOME/~}"
+  if [ $(echo -n $newPWD | awk -F '/' '{print NF}') -gt $num_dirs ]; then
+    newPWD=$(echo -n $newPWD | awk -F '/' '{print $1 "/.../" $(NF-1) "/" $(NF)}')
+  fi
+}
+
+PROMPT_COMMAND="calc_git; shortwd; $PROMPT_COMMAND"
 
 # Default Git enabled prompt with dirty state
-# export PS1="\u@\h \w \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
+#export PS1="\u@\h \w \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
+export PS1="\u@\h:\$newPWD \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
+#export PS1="\u@\h:\$newPWD \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\\n\$ "
 
 # Another variant:
 # export PS1="\[$bldgrn\]\u@\h\[$txtrst\] \w \[$bldylw\]\$git_branch\[$txtcyn\]\$git_dirty\[$txtrst\]\$ "
 
 # Default Git enabled root prompt (for use with "sudo -s")
-# export SUDO_PS1="\[$bakred\]\u@\h\[$txtrst\] \w\$ "
+export SUDO_PS1="\[$bakred\]\u@\h\[$txtrst\] \w\$ "
